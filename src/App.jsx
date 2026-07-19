3aec6295042d843852fe01b16e727a91f38405e8
saeed  GitHub <saeed.m6063@yahoo.com GitHub>
2026-07-19 17:45:53 +0300

    recat

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useEffect, useRef } from 'react'
-// import bodyHtml from './spaciaz-body.html?raw'
+// import Home from './pages/Home'
 // import {
 //   initSwipers,
 //   initCounters,
@@ -37,21 +37,77 @@
 //   }, [])
 
 //   return (
-//     <div
-//       ref={rootRef}
-//       className="spaciaz-root"
-//       dangerouslySetInnerHTML={{ __html: bodyHtml }}
-//     />
+//     <div ref={rootRef} className="spaciaz-root">
+//       <Home />
+//     </div>
 //   )
 // }
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NotFound from "./pages/NotFound";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import NotFound from "./pages/NotFound";
 
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import Home from "./pages/Home";
+
+
+
+
+
+// ظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظظ
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import {
+  initSwipers,
+  initCounters,
+  initStickyHeader,
+  initReadingProgress,
+  initScrollToTop,
+  initReveals,
+  initCursorButtons,
+  initParallax,
+  initMobileNav,
+} from "./behaviors";
+
+import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
+
 function App() {
+  
+useEffect(() => {
+  const root = document;
+
+  const swipers = initSwipers(root);
+
+  const cleanups = [
+    initCounters(root),
+    initStickyHeader(root),
+    initReadingProgress(root),
+    initScrollToTop(root),
+    initReveals(root),
+    initCursorButtons(root),
+    initParallax(root),
+    initMobileNav(root),
+  ];
+
+  return () => {
+    swipers.forEach((s) => {
+      try {
+        s.destroy(true, true);
+      } catch {}
+    });
+
+    cleanups.forEach((fn) => {
+      try {
+        fn?.();
+      } catch {}
+    });
+  };
+}, []);
+ 
   return (
 
     <BrowserRouter>
